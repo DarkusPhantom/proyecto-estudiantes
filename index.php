9389e704b8110c30afdbf9f6adf962b5e484347a
Darkus <luisfrodriguezb.lf@gmail.com>
2020-05-18 16:38:58 -0400

    He anexado estilos al encabezado de la tabla, añadi el aside y agregue contenido para la tabla y aside

--- index.php
+++ index.php
@@ -1,16 +1,90 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
 
+	<div class="contenedor">
+		<div class="contenido">
+			<div class="encabezado-tabla">
+				<h2>Estudiantes</h2>
+				<div class="button nuevo">
+					<a href="#">Nuevo</a>
+				</div>
+			</div>
+			<table>
+				<thead>
+					<tr>
+						<th>Cédula</th>
+						<th>Nombre</th>
+						<th>Apellido</th>
+						<th>Código</th>
+						<th>Materia</th>
+						<th>Notas</th>
+						<th>Turno</th>
+					</tr>
+				</thead>
 
-
-
-
-
-
-
-
-
-
+				<tbody>
+					<tr>
+						<td>12.345.678</td>
+						<td>Nombre Nombre</td>
+						<td>Apellido Apellido</td>
+						<td>CAO101</td>
+						<td>Nombre Materia</td>
+						<td>20</td>
+						<td>Dia</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+		
+					<tr>
+						<td>12.345.678</td>
+						<td>Nombre Nombre</td>
+						<td>Apellido Apellido</td>
+						<td>CAO101</td>
+						<td>Nombre Materia</td>
+						<td>20</td>
+						<td>Noche</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+		
+					<tr>
+						<td>12.345.678</td>
+						<td>Nombre Nombre</td>
+						<td>Apellido Apellido</td>
+						<td>CAO101</td>
+						<td>Nombre Materia</td>
+						<td>20</td>
+						<td>Dia</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+		
+					<tr>
+						<td>12.345.678</td>
+						<td>Nombre Nombre</td>
+						<td>Apellido Apellido</td>
+						<td>CAO101</td>
+						<td>Nombre Materia</td>
+						<td>20</td>
+						<td>Noche</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<aside>
+			<ul>
+				<li>Estudiates</li>
+				<li>Profesor</li>
+				<li>Materia</li>
+			</ul>
+		</aside>
+	</div>
 
 
 <?php include_once 'includes/templates/footer.php'; ?>

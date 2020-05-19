f78d1e1fb2795b7dcd3c49e727a09ef21499381d
Darkus <luisfrodriguezb.lf@gmail.com>
2020-05-19 14:19:14 -0400

    He finalizado con la pagina El footer, aside y su diseño ha sido finalizado (Me falta arreglar el footer por problemas del float:left del aside y contenido Siguente paso: JavaScript para la pagina

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,3 +1,8 @@
+	<footer class="clearfix">
+		<p>Todos los derechos reservados por Luis Rodriguez y Jhonattan Garcia &copy;</p>
+	</footer>
+	
+
 	<script></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -25,10 +25,10 @@
 				<tbody>
 					<tr>
 						<td>12.345.678</td>
-						<td>Nombre Nombre</td>
-						<td>Apellido Apellido</td>
-						<td>CAO101</td>
-						<td>Nombre Materia</td>
+						<td>Maria</td>
+						<td>Delgado</td>
+						<td>CAO102</td>
+						<td>Algoritmos I</td>
 						<td>20</td>
 						<td>Dia</td>
 						<td><a href="#" class="button editar">Editar</a></td>
@@ -37,11 +37,11 @@
 					</tr>
 		
 					<tr>
-						<td>12.345.678</td>
-						<td>Nombre Nombre</td>
-						<td>Apellido Apellido</td>
-						<td>CAO101</td>
-						<td>Nombre Materia</td>
+						<td>22.151.263</td>
+						<td>Jose Finita</td>
+						<td>Perez</td>
+						<td>TAO102</td>
+						<td>Elementos Discreto II</td>
 						<td>20</td>
 						<td>Noche</td>
 						<td><a href="#" class="button editar">Editar</a></td>
@@ -50,11 +50,11 @@
 					</tr>
 		
 					<tr>
-						<td>12.345.678</td>
-						<td>Nombre Nombre</td>
-						<td>Apellido Apellido</td>
+						<td>29.132.079</td>
+						<td>Nohemi</td>
+						<td>Heredia</td>
 						<td>CAO101</td>
-						<td>Nombre Materia</td>
+						<td>Fundamentos de programacion</td>
 						<td>20</td>
 						<td>Dia</td>
 						<td><a href="#" class="button editar">Editar</a></td>
@@ -63,25 +63,79 @@
 					</tr>
 		
 					<tr>
-						<td>12.345.678</td>
-						<td>Nombre Nombre</td>
-						<td>Apellido Apellido</td>
-						<td>CAO101</td>
-						<td>Nombre Materia</td>
+						<td>23.467.765</td>
+						<td>Franco</td>
+						<td>Silva</td>
+						<td>MAO104</td>
+						<td>Calculo Vectorial</td>
 						<td>20</td>
 						<td>Noche</td>
 						<td><a href="#" class="button editar">Editar</a></td>
 						<td><a href="#" class="button ver">Ver</a></td>
 						<td><a href="#" class="button eliminar">Eliminar</a></td>
 					</tr>
+
+					<tr>
+						<td>28.941.252</td>
+						<td>Joseph</td>
+						<td>Joestar</td>
+						<td>CAO101</td>
+						<td>Fundamentos de Algoritmos</td>
+						<td>20</td>
+						<td>Dia</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+
+					<tr>
+						<td>20.499.824</td>
+						<td>Brenda Noelle</td>
+						<td>Tovar Romero</td>
+						<td>PAO101</td>
+						<td>Programacion I</td>
+						<td>20</td>
+						<td>Noche</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+
+					<tr>
+						<td>22.000.231</td>
+						<td>Ricardo</td>
+						<td>Milos</td>
+						<td>PAO101</td>
+						<td>Programacion I</td>
+						<td>20</td>
+						<td>Dia</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+
+					<tr>
+						<td>24.089.422</td>
+						<td>Luis Miguel</td>
+						<td>Blanco</td>
+						<td>MAO101</td>
+						<td>Calculo I</td>
+						<td>20</td>
+						<td>Noche</td>
+						<td><a href="#" class="button editar">Editar</a></td>
+						<td><a href="#" class="button ver">Ver</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>
+
 				</tbody>
 			</table>
 		</div>
 		<aside>
-			<ul>
-				<li>Estudiates</li>
+			<ul class="barra-menu">
+				<li>Estudiantes</li>
 				<li>Profesor</li>
 				<li>Materia</li>
+				<li>Material de Apoyo</li>
 			</ul>
 		</aside>
 	</div>

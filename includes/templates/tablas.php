b17a506c51108de602ba9a01de010ab1d3e649c9
Darkus <luisfrodriguezb.lf@gmail.com>
2020-06-04 13:18:41 -0400

    Agregue tablas para organizar mejor el contenido y la seccion de cuestionarios que sera donde el usuario modificara o creara un nuevo usuario

---
+++ includes/templates/tablas.php
@@ -0,0 +1,159 @@
+<div id="contenido-tabla" class="ocultar">
+	<div class="encabezado-tabla">
+		<h2 id="nombre_tabla" class="nombre-tabla">Estudiantes</h2>
+	</div>
+	<div class="button registrar">
+		<a href="#">Registrar</a>
+	</div>
+	
+	<div class="buscador">
+		<label for="buscar-estudiante" class="busqueda-estudiante">Buscar:</label>
+		<input type="search" id="buscar-estudiante" name="buscar-estudiante" placeholder="Buscar...">
+	</div>
+
+	<table id="estudiantes" class="tabla_datos ocultar">
+		<thead>
+			<tr>
+				<th>Cédula</th>
+				<th>Nombre</th>
+				<th>Apellido</th>
+				<th>Edad</th>
+				<th>Sexo</th>
+				<th>Carrera</th>
+				<th>Semestre</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+				try {
+					require_once 'includes/functions/bd_conexion.php';
+						$sql = 'SELECT * FROM `alumno` ';
+							$resultado = $conn->query($sql);
+						} catch (Exception $e) {
+							$error = $e->getMessage();
+						}
+
+				while ($estudiantes = $resultado->fetch_assoc()): //Inicio While?> 
+					<tr>
+						<td><?php echo $estudiantes['ci']; ?></td>
+						<td><?php echo $estudiantes['nombre']; ?></td>
+						<td><?php echo $estudiantes['apellido']; ?></td>
+						<td><?php echo $estudiantes['edad']; ?></td>
+						<td><?php echo $estudiantes['sexo']; ?></td>
+						<td><?php echo $estudiantes['carrera']; ?></td>
+						<td><?php echo $estudiantes['semestre']; ?></td>
+						<td><a href="editar.php" class="button editar">Editar</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>		 
+			<?php endwhile; //Fin While ?>				
+		</tbody>
+	</table>
+
+
+	<table id="profesores" class="tabla_datos ocultar">
+		<thead>
+			<tr>
+				<th>Cédula</th>
+				<th>Nombre</th>
+				<th>Apellido</th>
+				<th>Sexo</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+				try {
+					require_once 'includes/functions/bd_conexion.php';
+					$sql = 'SELECT * FROM `docente` ';
+					$resultado = $conn->query($sql);
+				} catch (Exception $e) {
+					$error = $e->getMessage();
+				}
+
+
+				while ($docentes = $resultado->fetch_assoc()): //Inicio While?> 
+					<tr>
+						<td><?php echo $docentes['ci-d']; ?></td>
+						<td><?php echo utf8_encode($docentes['nombre-d']); ?></td>
+						<td><?php echo utf8_encode($docentes['apellido-d']); ?></td>
+						<td><?php echo $docentes['sexo-d']; ?></td>
+						<td><a href="editar.php" class="button editar">Editar</a></td>
+						<td><a href="#" class="button eliminar">Eliminar</a></td>
+					</tr>		 
+			<?php  endwhile; //Fin While ?>				
+		</tbody>
+	</table>
+
+
+	<table id="asignaturas" class="tabla_datos ocultar">
+		<thead>
+			<tr>
+				<th>Código</th>
+				<th>Asignatura</th>
+				<th>Semestre</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+				try {
+					require_once 'includes/functions/bd_conexion.php';
+					$sql = 'SELECT * FROM `curso` ';
+					$resultado = $conn->query($sql);
+				} catch (Exception $e) {
+					$error = $e->getMessage();
+				}
+
+
+				while ($cursos = $resultado->fetch_assoc()): //Inicio While?> 
+					<tr>
+						<td><?php echo $cursos['codigo']; ?></td>
+						<td><?php echo utf8_encode($cursos['nombre-c']); ?></td>
+						<td><?php echo $cursos['nivel']; ?></td>
+					</tr>		 
+			<?php endwhile; //Fin While ?>				
+		</tbody>
+	</table>
+
+	<table id="notas" class="tabla_datos ocultar">
+		<thead>
+			<tr>
+				<th>Cédula</th>
+				<th>Código</th>
+				<th>Calificacion</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+				try {
+					require_once 'includes/functions/bd_conexion.php';
+					$sql = 'SELECT * FROM `nota` ';
+					$resultado = $conn->query($sql);
+				} catch (Exception $e) {
+					$error = $e->getMessage();
+				}
+
+
+				while ($notas = $resultado->fetch_assoc()): //Inicio While?> 
+					<tr>
+						<td><?php echo $notas['ci-a']; ?></td>
+						<td><?php echo $notas['codigo-c']; ?></td>
+						<td><?php echo $notas['calificacion']; ?></td>
+					</tr>		 
+			<?php endwhile; //Fin While ?>				
+		</tbody>
+	</table>
+
+	<div class="pagination">
+		<a href="#">&laquo;</a>
+		<a class="active" href="#">1</a>
+		<a href="#">2</a>
+		<a href="#">3</a>
+		<a href="#">4</a>
+		<a href="#">5</a>
+		<a href="#">6</a>
+		<a href="#">&raquo;</a>
+	</div>
+</div>

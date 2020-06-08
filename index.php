ec56c818baa197d67a7c79af29ade256f9c1be63
Darkus <luisfrodriguezb.lf@gmail.com>
2020-06-08 15:00:12 -0400

    Prepare el espacio para introducir el slider del index

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
 	<div class="contenedor">
+		<div class="cuadrito">
+			<img src="" alt="">
+		</div>
+
 		<div class="contenido" id="info-principal">
 			<h2>Bienvenido a la Universidad de Carabobo</h2>
 			<p class="presentacion">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>

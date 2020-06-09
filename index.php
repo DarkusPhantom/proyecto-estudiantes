df551c35122f33df55d05e4c6f1a1e453b80cd21
Darkus <luisfrodriguezb.lf@gmail.com>
2020-06-08 23:12:28 -0400

    Le di estilos al index. Falta colocar el slider y unas imagenes para finalizar

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
 	<div class="contenedor">
-		<div class="contenido">
-			<div id="cuadrito">
+		<div class="contenido" id="inicio">
+			<h2>Bienvenido a la Universidad de Carabobo</h2>
+			<div id="slidebar">
 				<img src="" alt="">
 			</div>
-			
+			<!--<div id="barrita">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>-->
 			<div id="info-principal">
-				<h2>Bienvenido a la Universidad de Carabobo</h2>
-				<p class="presentacion">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>
-				<p class="presentacion">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>
-				<p class="presentacion">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>
+				<div class="presentacion">
+					<h3>titulo 1</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>
+				</div>
+				<div class="presentacion">
+					<h3>titulo 2</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>
+				</div>
+				<div class="presentacion">
+					<h3>titulo 3</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam neque non blanditiis at consectetur sint aspernatur consequuntur, sequi illo ex praesentium ullam similique quo distinctio earum autem, vel placeat architecto?</p>
+				</div>
 			</div>
 		</div>
 
